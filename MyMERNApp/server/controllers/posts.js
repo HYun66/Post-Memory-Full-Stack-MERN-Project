bdd200bd2b0de726b029a2fa01eb3139dedbeb7f
HYun66 <niuhy0728@gmail.com>
2020-12-28 18:15:46 +1100

    add main part of project

---
+++ MyMERNApp/server/controllers/posts.js
@@ -0,0 +1,37 @@
+// handle handlers for routes
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPosts = (req, res) => {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+  try {
+    newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  // check if _id is mongoose id
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post with that id.");
+
+  const updatePost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  });
+
+  res.json(updatePost);
+};
